d0dc98dd9c653d3c15a1b8d8fb92e3e1efcdf2e0
sock745 <116982908+sock745@users.noreply.github.com>
2024-07-02 11:49:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,29 @@ pepe1 = pygame.image.load("angrypepe.png")
 pepe_rect1 = pepe1.get_rect()
 pepe_rect1.x=pepespwnx1
 pepe_rect1.y=pepespwny1
+
+bg = pygame.image.load("gameover.png")
+
+speed = 5
+
+def draw():
+  screen.blit(alex,(alex_rect.x,alex_rect.y))
+  screen.blit(pepe,(pepe_rect.x,pepe_rect.y))
+  screen.blit(pepe1,(pepe_rect1.x,pepe_rect1.y))
+  pygame.display.update()
+
+def quit_game():
+  global run
+  for event in pygame.event.get():
+    if alex_rect.colliderect(pepe_rect):
+      run = False
+    elif alex_rect.colliderect(pepe_rect1):
+      run = False  
+run = True
+
+while run:
+  clock.tick(30)
+  screen.fill((248,236,209))
+  
+  draw()
+  quit_game()
